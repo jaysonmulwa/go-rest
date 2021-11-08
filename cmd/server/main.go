1313493a6882104447d9d0c23db539a545f9b120
jaysonmulwa <jayson.mulwa@gmail.com>
2021-11-08 22:54:18 +0300

    Structuring our application

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,6 +2,21 @@ package main
 
 import "fmt"
 
+//Struct contains ponters to database
+type App struct {
+}
+
+//Run sets up our application
+func (app *App) Run() error {
+	fmt.Println("Setting up our app")
+	return nil
+}
+
 func main() {
 	fmt.Println("Jayson Worlds")
+	app := App{}
+	if err := app.Run(); err != nil {
+		fmt.Println("Err")
+		fmt.Println(err)
+	}
 }
